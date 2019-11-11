e69e8fd7eee3420bf4c2041c6088d77e0ceb2dd6
omni-primus <56962599+omni-primus@users.noreply.github.com>
2019-11-11 15:12:30 +0100

    nav entfernt, checkbox eingefügt

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <a href="javascript:void(0);" class="nav-icon" onclick="shownav()">
-        <i class="fa fa-bars" aria-hidden="true"></i>
-      </a>
-      <div id="links" style="display: block;">
-        <router-link class="test" to="/">Home</router-link> |
-        <router-link class="test" to="/about">About</router-link> |
-        <router-link class="test" to="/Mannschaft">Mannschaft</router-link>
-      </div>
-    </div>
     <router-view />
   </div>
 </template>
@@ -21,35 +11,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  overflow: hidden;
-  background-color: rgb(9, 87, 2);
-  width: 100%;
-  position: fixed;
-  z-index: 10000;
-  top: 0;
-  display: block;
-  a {
-    float: left;
-    display: block;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-    color: #293038;
-    &.router-link-exact-active {
-      color: #11ca30;
-    }
-  }
-  #links {
-    position: relative;
-  }
-  #links a {
-    width: 100%;
-    color: black;
-  }
 }
 </style>
 
@@ -65,14 +26,3 @@
   margin-top: 100px;
 }
 </style>
-
-<script>
-  function shownav() {
-    var x = document.getElementById("links");
-    if(x.style.display === "block"){
-      x.style.display = "none";
-    }else{
-      x.style.display = "block";
-    }
-  }
-</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,62 @@
 <template>
   <div class="home">
     <start msg="Fußball Ticker App" />
+    <div id="selection">
+      <details id="bundesliga">
+        <summary>Bundesliga</summary>
+        <input id="FCB" type="checkbox" name="FCB" />
+        <label for="FCB">1.FC Bayern München</label>
+        <br />
+        <input id="BVB" type="checkbox" name="BVB" />
+        <label for="BVB">Borussia Dortmund</label>
+        <br />
+        <input id="UB" type="checkbox" name="UB" />
+        <label for="UB">1.FC Union Berlin</label>
+        <br />
+        <input id="BMG" type="checkbox" name="BMG" />
+        <label for="BMG">Borussia Mönchen Gladbach</label>
+        <br />
+        <input id="RBL" type="checkbox" name="RBL" />
+        <label for="RBL">RB Leipzig</label>
+        <br />
+        <input id="SCF" type="checkbox" name="SCF" />
+        <label for="SCF">SC Freiburg</label>
+        <br />
+        <input id="TSGH" type="checkbox" name="TSGH" />
+        <label for="TSGH">TSG 1899 Hoffenheim</label>
+        <br />
+        <input id="SGEF" type="checkbox" name="SGEF" />
+        <label for="SGEF">SG Eintracht Frankfurt</label>
+        <br />
+        <input id="FCS04" type="checkbox" name="FCS04" />
+        <label for="FCS04">FC Schalke 04</label>
+        <br />
+        <input id="BL" type="checkbox" name="BL" />
+        <label for="BL">Bayer 04 Leverkusen</label>
+        <br />
+        <input id="VFLW" type="checkbox" name="VFLW" />
+        <label for="VFLW">VFL Wolfsburg</label>
+        <br />
+        <input id="HBSC" type="checkbox" name="HBSC" />
+        <label for="HBSC">Hertha BSC</label>
+        <br />
+        <input id="FD" type="checkbox" name="FD" />
+        <label for="FD">Fortuna Düsseldorf</label>
+        <br />
+        <input id="WB" type="checkbox" name="WB" />
+        <label for="WB">Sv Werder Bremen</label>
+        <br />
+        <input id="FCA" type="checkbox" name="FCA" />
+        <label for="FCA">FC Augsburg</label>
+        <br />
+        <input id="M05" type="checkbox" name="M05" />
+        <label for="M05">1. FSV Mainz 05</label>
+        <br />
+        <input id="FCK" type="checkbox" name="FCK" />
+        <label for="FCK">1. FC Köln</label>
+        <br />
+      </details>
+    </div>
   </div>
 </template>
 
