81f72a8bf5c37c3ce2d072dc10f0bc8e3e7878e0
omni-primus <56962599+omni-primus@users.noreply.github.com>
2019-11-11 13:54:46 +0100

    Synchronisation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/Mannschaft">Mannschaft</router-link>
+      <a href="javascript:void(0);" class="nav-icon" onclick="shownav()">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+      </a>
+      <div id="links" style="display: block;">
+        <router-link class="test" to="/">Home</router-link> |
+        <router-link class="test" to="/about">About</router-link> |
+        <router-link class="test" to="/Mannschaft">Mannschaft</router-link>
+      </div>
     </div>
     <router-view />
   </div>
@@ -19,12 +24,31 @@
 }
 #nav {
   padding: 30px;
+  overflow: hidden;
+  background-color: rgb(9, 87, 2);
+  width: 100%;
+  position: fixed;
+  z-index: 10000;
+  top: 0;
+  display: block;
   a {
-    font-weight: bold;
+    float: left;
+    display: block;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
     color: #293038;
     &.router-link-exact-active {
       color: #11ca30;
     }
+  }
+  #links {
+    position: relative;
+  }
+  #links a {
+    width: 100%;
+    color: black;
   }
 }
 </style>
@@ -32,10 +56,23 @@
 <style scoped>
 .home {
   color: #000;
+  margin-top: 100px;
 }
 .mannschaft {
   color: #000;
   height: 100px;
   background-color: #20d164;
+  margin-top: 100px;
 }
 </style>
+
+<script>
+  function shownav() {
+    var x = document.getElementById("links");
+    if(x.style.display === "block"){
+      x.style.display = "none";
+    }else{
+      x.style.display = "block";
+    }
+  }
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,9 @@
     <h1>Test Test, lelel</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About"
+};
+</script>
